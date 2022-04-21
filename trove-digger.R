c5583774b8072c68d9c958fad661be6ac3f1096f
mickmorrison <mmorrisonau@gmail.com>
2022-04-21 13:13:04 +1000

    Update trove-digger.R

--- trove-digger.R
+++ trove-digger.R
@@ -8,41 +8,33 @@
 library(httr)
 library(tidyverse)
 library(rlist)
+library(repurrsive)
+library(dplyr)
 
 # Use httr GET function to access the API with a search query
 # this URL needs to be modified to suit the query you wish to run. Queries can be very complex 
 # see https://trove.nla.gov.au/about/create-something/using-api/api-technical-guide#parameters-available-when-searching
 
 query <- GET("https://api.trove.nla.gov.au/v2/result?key=l5c9j180i2m9ngs2&zone=newspaper&q=weipa%20mission")
-query
-query_results <- content(get,as = "parsed")
+
+query_results <- content(query,as = "text")
+
+# Turn this into a function if poss.
+# some trial code for this
+# trove-digger <- function {query <- (GET("https://api.trove.nla.gov.au/v2/result?key=l5c9j180i2m9ngs2&zone=newspaper&q=weipa%20mission"))
 
 # Seeing what was gotten
-query_results 
+names(query_results)
+class(query_results)
+query_results
+str(query_results)
+head(query_results)
 
-# this has parsed a long list of fields. Viewing as a data frame just lists 272 variables in one column. We need to break into columns
-# now to try to extract the structured text
+# Lots of data here, but as a series of nested lists. annoying
 
-#first let's look at the structure of the response and identify which list to target
-str(content(query), max.level = 6
-    )
+# convert query results to df to use 'rectangling' approach
+df <- tibble(query_results)
 
-# so the first list is of Trove Zones
-# second list contains records that we want
+# the data is served back as json text mode, so decided to look into that as an option more.
+GET("https://api.trove.nla.gov.au/v2/result?key=l5c9j180i2m9ngs2&zone=newspaper&q=weipa%20mission")
 
-# Next step is to store the list we want
-query_content <- list.select(get$content)
-
-# check str of this list
-str(query_content) #This confirms we have lists in this dataset. After some probing of each of the lists in the previous command, it seems clear that content is the container for the data we want.
-
-# This outputs a massive list, I think of all the records returned (11000). Now trying to use some different functions to access this data
-
-
-
-
-# Acknowledgements
-
-# The following sources have been very helpful in producing this program
-## The wonderful Trove.nla.gov.auu API! https://trove.nla.gov.au/about/create-something/using-api/api-technical-guide
-## https://tidyr.tidyverse.org/articles/rectangle.html excellent overview of rectangling
