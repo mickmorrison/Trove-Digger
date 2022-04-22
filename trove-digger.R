568229f9315b2e419201341c1cd859d9760befa8
mickmorrison <mmorrisonau@gmail.com>
2022-04-22 12:47:01 +1000

    querying the output
    Trying to access the JSON, not getting far. Trying to find out what server is returning

--- trove-digger.R
+++ trove-digger.R
@@ -9,28 +9,37 @@ library(httr)
 library(tidyverse)
 library(rlist)
 library(RJSONIO)
+library(jsonlite)
+
 
 # Use httr GET function to access the API with a search query
 # this URL needs to be modified to suit the query you wish to run. Queries can be very complex 
 # see https://trove.nla.gov.au/about/create-something/using-api/api-technical-guide#parameters-available-when-searching
 
-query <- GET("https://api.trove.nla.gov.au/v2/result?key=l5c9j180i2m9ngs2&zone=newspaper&q=weipa%20mission")
+query <- GET("https://api.trove.nla.gov.au/v2/result?key=l5c9j180i2m9ngs2&zone=newspaper&q=weipa%20mission&encoding=json&n=10")
 
-query_results <- content(query,as = "text")
+# commenting out to simplify code and work directly with api
+# query_results <- content(query,as = "text")
 
 # Turn this into a function if poss.
 # some trial code for this
 # trove-digger <- function {query <- (GET("https://api.trove.nla.gov.au/v2/result?key=l5c9j180i2m9ngs2&zone=newspaper&q=weipa%20mission"))
 
 # Seeing what was gotten
-names(query_results)
-class(query_results)
-query_results
-str(query_results)
-head(query_results)
+names(query)
+class(query)
+query
+str(query)
+head(query)
 
 # the data is served back as json text mode, heavily nested and tricky to work with. 
-# Looking at that as an option
 
-# Using RJSONIO to see if the 
-resp <- fromJSON("https://api.trove.nla.gov.au/v2/result?key=l5c9j180i2m9ngs2&zone=newspaper&q=weipa%20mission")
+
+# data I want is in a nested list at:
+# resp[["response"]][["zone"]][[1]][["records"]][["article"]]
+# But how to get it?
+
+#try to access as a list
+list.select(resp,"article") 
+
+
