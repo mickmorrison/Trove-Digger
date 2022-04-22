b3d1fc7f4afe5305fc7d81fcc9c3fe70aaee8f80
mickmorrison <mmorrisonau@gmail.com>
2022-04-22 15:08:42 +1000

    extracted functional data
    script returns a matrix with specific records from

--- trove-digger.R
+++ trove-digger.R
@@ -11,35 +11,58 @@ library(rlist)
 library(RJSONIO)
 library(jsonlite)
 
+rm(list=ls())
 
 # Use httr GET function to access the API with a search query
 # this URL needs to be modified to suit the query you wish to run. Queries can be very complex 
 # see https://trove.nla.gov.au/about/create-something/using-api/api-technical-guide#parameters-available-when-searching
+# i've just followed my nose here
+# this command creates a df from the above URL]
 
-query <- GET("https://api.trove.nla.gov.au/v2/result?key=l5c9j180i2m9ngs2&zone=newspaper&q=weipa%20mission&encoding=json&n=10")
+searchurl <- "https://api.trove.nla.gov.au/v2/result?key=l5c9j180i2m9ngs2&zone=newspaper&q=weipa%20mission&n=10&encoding=json"
 
-# commenting out to simplify code and work directly with api
-# query_results <- content(query,as = "text")
+resp <- GET(searchurl)
 
-# Turn this into a function if poss.
-# some trial code for this
-# trove-digger <- function {query <- (GET("https://api.trove.nla.gov.au/v2/result?key=l5c9j180i2m9ngs2&zone=newspaper&q=weipa%20mission"))
+#First test to see http type
+http_type(resp)
 
-# Seeing what was gotten
-names(query)
-class(query)
-query
-str(query)
-head(query)
+# GET the http response from trove api endpoint
+content <- (content(resp))
 
-# the data is served back as json text mode, heavily nested and tricky to work with. 
+content
+#view the results
+content # is returning nice lists - query results present. But weird and twisted format. 
+
+# Display the structure
+str(content)
+
+# list
+# attempt to coerce list into columns
+# specifying the level of list I'm interested in
+
+records <- as.data.frame(content$response)
+
+# so this has helped transform the data
+# https://stackoverflow.com/questions/48212759/extracting-data-from-an-api-using-r
 
 
-# data I want is in a nested list at:
-# resp[["response"]][["zone"]][[1]][["records"]][["article"]]
-# But how to get it?
+##### This section might be better above ####
+## Here be demons and fairies
 
-#try to access as a list
-list.select(resp,"article") 
+list.load(resp)
 
+text <- list.load("https://api.trove.nla.gov.au/v2/result?key=l5c9j180i2m9ngs2&zone=newspaper&q=weipa%20mission&n=10&encoding=json")
 
+class(text)
+
+# Testing this with jsonlite
+library(jsonlite)
+query <- GET("https://api.trove.nla.gov.au/v2/result?key=l5c9j180i2m9ngs2&zone=newspaper&q=weipa%20mission&n=10&encoding=json")
+
+r2 <- rawToChar(query$content)
+
+class(r2)
+
+r3 <- fromJSON(r2)
+df <- as.data.frame(r3$response$zone$records$article)
+df
