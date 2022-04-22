b614c318584c80a51a8b8bd78063365b17ad69bc
mickmorrison <mmorrisonau@gmail.com>
2022-04-22 12:22:00 +1000

    Changing encoding
    Adding information to specify that trove should return a json encoded file

--- trove-digger.R
+++ trove-digger.R
@@ -8,8 +8,7 @@
 library(httr)
 library(tidyverse)
 library(rlist)
-library(repurrsive)
-library(dplyr)
+library(RJSONIO)
 
 # Use httr GET function to access the API with a search query
 # this URL needs to be modified to suit the query you wish to run. Queries can be very complex 
@@ -30,11 +29,8 @@ query_results
 str(query_results)
 head(query_results)
 
-# Lots of data here, but as a series of nested lists. annoying
+# the data is served back as json text mode, heavily nested and tricky to work with. 
+# Looking at that as an option
 
-# convert query results to df to use 'rectangling' approach
-df <- tibble(query_results)
-
-# the data is served back as json text mode, so decided to look into that as an option more.
-GET("https://api.trove.nla.gov.au/v2/result?key=l5c9j180i2m9ngs2&zone=newspaper&q=weipa%20mission")
-
+# Using RJSONIO to see if the 
+resp <- fromJSON("https://api.trove.nla.gov.au/v2/result?key=l5c9j180i2m9ngs2&zone=newspaper&q=weipa%20mission")
